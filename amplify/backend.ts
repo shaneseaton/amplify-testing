8995ccd178c9f83641db5fe59358d33147045617
Shane Seaton <sea066@csiro.au>
2024-05-12 20:35:26 +1000

    adding storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 // import { data } from './data/resource';
-// import { storage } from './storage/resource';
+import { storage } from './storage/resource';
 
 const backend = defineBackend({
   auth,
   // data,
-  // storage
+  storage
 });
 
 // backend.addOutput({
