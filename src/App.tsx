4a0bce1f1d1427cc71ad69916fb033b002b81e7a
Shane Seaton <sea066@csiro.au>
2024-05-12 14:41:06 +1000

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <Authenticator loginMechanisms={['email']} hideSignUp>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <p>Farts</p>
   );
 }
 
