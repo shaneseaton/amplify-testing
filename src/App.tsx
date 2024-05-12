290b4061b71c2bc29289b4868c09c32965209bbe
Shane Seaton <sea066@csiro.au>
2024-05-12 20:55:55 +1000

    addsigning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <p>Farts</p>
+    <Authenticator loginMechanisms={['email']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
