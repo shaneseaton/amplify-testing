cb8ffb977286c895c002b3dcf8bc8d6a8fc9d28c
Shane Seaton <sea066@csiro.au>
2024-05-20 10:49:53 +1000

    more auth play

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,21 +1,24 @@
 import { defineStorage } from '@aws-amplify/backend';
 
+const allgroups = ["ADMIN", "TEAM1", "TEAM2"]
+
 export const storage = defineStorage({
   name: 'seatontestingamplify',
   access: (allow) => ({
-    "shared/*": [ 
-      allow.authenticated.to(["read", "write", "delete"])
+    "data/*": [ 
+      allow.authenticated.to(["read", "write", "delete"]),
+      allow.groups(["ADMIN"]).to(["read", "write", "delete"]),
+      allow.groups(["TEAM1", "TEAM2"]).to(["read"]),
     ],
-    // "data/shared/*": [ 
-    //   allow.authenticated.to(["read", "write", "delete"])
-    // ],
-    // "data/team1/*": [
-    //   allow.groups(["ADMIN"]).to(["read", "write", "delete"]),
-    //   allow.groups(["TEAM1"]).to(["read", "write", "delete"]),
-    // ],
-    // "data/team2/*": [
-    //   allow.groups(["ADMIN"]).to(["read", "write", "delete"]),
-    //   allow.groups(["TEAM2"]).to(["read", "write", "delete"]),
-    // ]
+    "data/shared/*": [ 
+      allow.authenticated.to(["read", "write", "delete"]),
+      allow.groups(allgroups).to(["read", "write", "delete"]),
+    ],
+    "data/team1/*": [
+      allow.groups(["ADMIN", "TEAM1"]).to(["read", "write", "delete"]),
+    ],
+    "data/team2/*": [
+      allow.groups(["ADMIN", "TEAM2"]).to(["read", "write", "delete"]),
+    ]
   })
 });
